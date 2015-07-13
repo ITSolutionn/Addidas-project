93c258e9b9c8b5ada930ad20dc39109b37ec6fef
amit <amit.prajapati16007@gmail.com>
2015-07-13 21:41:58 +0530

    First demo upload

---
+++ AddidasDemo/Addidas.Web/Addidas.Web/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Addidas.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Shop()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Single()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ AddidasDemo/Addidas.Web/Addidas.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Addidas.Web.MvcApplication" Language="C#" %>
