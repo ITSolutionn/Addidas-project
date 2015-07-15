48ae5e6aa81d28b426ae8394af79b8b6cedcc863
Akshay Chawla <akshay.chawla@hotmail.com>
2015-07-16 00:05:33 +0530

    Model for product and account class
    Model for product and account class.

---
+++ AddidasDemo/Addidas.Model/Account.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addidas.Model
+{
+    class Account
+    {
+    }
+    /// <summary>
+    /// Model class for User Registeration
+    /// </summary>
+    public class UserRegisteration
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Address { get; set; }
+        public string Country { get; set; }
+        public int Contactno { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDelete { get; set; }
+    }   
+    /// <summary>
+    /// Model class for Login
+    /// </summary>
+    public class Login
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ AddidasDemo/Addidas.Model/Product.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addidas.Model
+{
+    public class Product
+    {
+    }
+
+    /// <summary>
+    /// Model Class for Product Category Master
+    /// </summary>
+    public class CategoryMaster
+    {
+        public int CategoryMasterID { get; set; }
+        public string CategoryName { get; set; }
+    }
+    /// <summary>
+    /// Model class for Product Category Master
+    /// </summary>
+    public class ColorMaster
+    {
+        public int ColorMasterID { get; set; }
+        public string ColorName { get; set; }
+    }
+    /// <summary>
+    /// Model class for Product Master
+    /// </summary>
+    public class ProductMaster
+    {
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public int Price { get; set; }
+        public int Size { get; set; }
+        public int Rating { get; set; }
+        public int ColorMasterID { get; set; }
+        public int NoOfCounts { get; set; }
+    }
+    /// <summary>
+    /// Model class for product category-which product is of which category
+    /// </summary>
+    public class ProductCategory
+    {
+        public int CategoryMasterID { get; set; }
+        public int ProductMasterID { get; set; }
+    }
+    /// <summary>
+    /// Model class for Product Image
+    /// </summary>
+    public class ProductImage
+    {
+        public string ProductMasterID { get; set; }
+        public string ImageName { get; set; }
+        public string ImagePath { get; set; }
+    }
+
+}
