b194658abe7b29d585da346e4c1abb763f071c72
amit <amit.prajapati16007@gmail.com>
2015-07-19 22:43:35 +0530

    bhai cush kam karo

--- AddidasDemo/Addidas.Model/Account.cs
+++ AddidasDemo/Addidas.Model/Account.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,12 +15,19 @@ namespace Addidas.Model
     /// </summary>
     public class UserRegisteration
     {
+        [Required]
+        [StringLength(50)]
+        [Display(Description = "First Name")]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]        
         public string Name { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
         public string Email { get; set; }
         public string Password { get; set; }
         public string Address { get; set; }
         public string Country { get; set; }
-        public int Contactno { get; set; }
+        public int? Contactno { get; set; }
         public bool IsActive { get; set; }
         public bool IsDelete { get; set; }
     }   
