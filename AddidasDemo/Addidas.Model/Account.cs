d19ae6d7e19142521584a0c06d9060ca3590f980
Akshay Chawla <akshay.chawla@hotmail.com>
2015-07-21 23:39:09 +0530

    Modified model property validation
    For password.

--- AddidasDemo/Addidas.Model/Account.cs
+++ AddidasDemo/Addidas.Model/Account.cs
@@ -18,7 +18,7 @@ namespace Addidas.Model
         [Required]
         [StringLength(50)]
         [Display(Description = "First Name")]
-        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]        
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
         public string Name { get; set; }
         [Required]
         [DataType(DataType.EmailAddress)]
@@ -28,6 +28,8 @@ namespace Addidas.Model
         [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
+        [RegularExpression(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$", ErrorMessage = "Use letters only please")]
+        //[RegularExpression(@"^(?=.*\d)(?=.*[a-zA-Z]).{4,8}$", ErrorMessage = "Password is wrong")]
         public string Password { get; set; }
         [Required(ErrorMessage = "Please Enter Confirm Password")]
         [StringLength(50, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
@@ -39,19 +41,19 @@ namespace Addidas.Model
         public string Country { get; set; }
         [Required(ErrorMessage = "Please Enter Mobile No")]
         [Display(Name = "Mobile")]
-        [StringLength(10, ErrorMessage = "The Mobile must contains 10 characters", MinimumLength = 10)]        
+        [StringLength(10, ErrorMessage = "The Mobile must contains 10 characters", MinimumLength = 10)]
         public string ContactNo { get; set; }
         public bool IsActive { get; set; }
         public bool IsDelete { get; set; }
 
-    }   
+    }
     /// <summary>
     /// Model class for Login
     /// </summary>
     public class Login
     {
         public string Email { get; set; }
-        
+
         //[RegularExpression("[0-9a-zA-Z@#$%^&*()_+}{:?><,.*-]",ErrorMessageResourceType = typeof(AuthenticationModelsResource),ErrorMessageResourceName = "PasswordNotStrongEnoughMessage")]
         public string Password { get; set; }
     }
